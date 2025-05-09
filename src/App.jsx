fc56b7f3c37bdfa9abab94b7672be585b199f44d
and-cuau <98066177+and-cuau@users.noreply.github.com>
2025-05-08 23:51:43 -0400

    Fixed home page links spacing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react';
 import React from 'react';
 import './App.css';
 
-// 🟢 Change starts here
+
 import { HashRouter as Router, Routes, Route, Link, useLocation, Navigate } from "react-router-dom";
-// 🟢 Change ends here
 
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects";
 import AboutMe from "./components/AboutMe";
 
 
-// Wrapper to expose `useLocation` inside HashRouter
 function AppContent() {
   const [count, setCount] = useState(0)
 
@@ -29,7 +27,7 @@ function AppContent() {
         <div className="test"></div>
         <div className="links">
           <Link className={`link ${isWhiteLink ? 'link-white' : 'link-black'}`} to="/aboutme">About</Link>
-          <Link className={`link ${isWhiteLink ? 'link-white' : 'link-black'}`} to="/projects">Projects</Link>
+          <Link className={`link right ${isWhiteLink ? 'link-white' : 'link-black'}`} to="/projects">Projects</Link>
         </div>
       </nav>
 
